6312dbf0393e3bf08d38c70b846d9b697615ec30
miketvo <ikxplicitie@gmail.com>
2023-04-17 15:27:44 +0700

    Refactored quang_algo1.SecretKeyGuesser.java

--- src/main/java/vn/rmit/cosc2658/quang_algo1/SecretKeyGuesser.java
+++ src/main/java/vn/rmit/cosc2658/quang_algo1/SecretKeyGuesser.java
@@ -4,82 +4,51 @@ import vn.rmit.cosc2658.SecretKey;
 
 
 public class SecretKeyGuesser {
-    private static final String[] POSSIBLE_VALUES = {"R", "M", "I", "T"};
+    private static final char[] POSSIBLE_VALUES = "RMIT".toCharArray();
     private static final int LENGTH = 16;
 
-    public static void start(SecretKey secretKey) {
-        //create a list to store numbers of each element in the correct key
-        int[] numMatchesList = new int[4];
-        //first guess to count the numbers of R in correct key
-        String firstGuess = "RRRRRRRRRRRRRRRR";
-        int numMatches = secretKey.guess(firstGuess);
-        numMatchesList[0] = numMatches;
-        if (numMatches == LENGTH) {
-            System.out.println("The secret key is :" + firstGuess);
-            return;
-        }
-        //second guess to count the number of M in correct key
-        String secondGuess = "MMMMMMMMMMMMMMMM";
-        numMatches = secretKey.guess(secondGuess);
-        numMatchesList[1] = numMatches;
-        if (numMatches == LENGTH) {
-            System.out.println("The secret key is :" + secondGuess);
-            return;
-        }
-        //third guess to count the number of I in correct key
-        String thirdGuess = "IIIIIIIIIIIIIIII";
-        numMatches = secretKey.guess(thirdGuess);
-        numMatchesList[2] = numMatches;
-        if (numMatches == LENGTH) {
-            System.out.println("The secret key is :" + secondGuess);
-            return;
-        }
-        //fourth guess to count the number of T in correct key
-        String fourthGuess = "TTTTTTTTTTTTTTTT";
-        numMatches = secretKey.guess(fourthGuess);
-        numMatchesList[3] = numMatches;
-        if (numMatches == LENGTH) {
-            System.out.println("The secret key is :" + secondGuess);
-            return;
-        }
+    public static void start(SecretKey sk) {
+        // Count the number of occurrences for each character in the secret key
+        int[] letterCount = new int[4];
+        letterCount[0] = sk.guess("RRRRRRRRRRRRRRRR");
+        letterCount[1] = sk.guess("MMMMMMMMMMMMMMMM");
+        letterCount[2] = sk.guess("IIIIIIIIIIIIIIII");
+        letterCount[3] = sk.guess("TTTTTTTTTTTTTTTT");
+
+
         // create a guess with exact number of each element in correct key
-        String guess = "";
-        int i = 0;
-        int numMatchesListIdx = numMatchesList[0];
-        while (i < numMatchesList.length) {
-            if (numMatchesListIdx > 0) {
-                guess = guess.concat(POSSIBLE_VALUES[i]);
-                numMatchesListIdx--;
-            } else {
-                i++;
-                if (i < 4) numMatchesListIdx = numMatchesList[i];
+        StringBuilder guess = new StringBuilder();
+        for (int letter = 0; letter < POSSIBLE_VALUES.length; letter++) {
+            while (letterCount[letter] > 0) {
+                guess.append(POSSIBLE_VALUES[letter]);
+                letterCount[letter]--;
             }
         }
-        // make guesses
-        while (secretKey.guess(guess) != 16) {
-            guess = generateGuess(guess, secretKey);
+
+        while (sk.guess(guess.toString()) != 16) {  // make guesses
+            guess = new StringBuilder(generateGuess(guess.toString(), sk));
             System.out.println("Guessing... " + guess);
         }
     }
 
-    static int order(char c) {
-        if (c == 'R') {
-            return 0;
-        } else if (c == 'M') {
-            return 1;
-        } else if (c == 'I') {
-            return 2;
-        }
-        return 3;
+
+    private static int toInt(char c) {
+        return switch (c) {
+            case 'R' -> 0;
+            case 'M' -> 1;
+            case 'I' -> 2;
+            case 'T' -> 3;
+            default -> throw new IllegalStateException("Unexpected value: " + c);
+        };
     }
 
-    public static String generateGuess(String guess, SecretKey secretKey) {
+    private static String generateGuess(String guess, SecretKey secretKey) {
         char[] curr = guess.toCharArray();
         char[] test = curr.clone();
         int i = 0;
         while (i < LENGTH) {
             for (int j = 0; j < LENGTH; j++) {
-                if (order(test[i]) != order(test[j])) {
+                if (toInt(test[i]) != toInt(test[j])) {
                     // increase this one and stop
                     char temp = test[i];
                     test[i] = test[j];
