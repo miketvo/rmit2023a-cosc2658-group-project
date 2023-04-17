c6512cc91ba8473a6e3a323d7feba88f7633ffd3
miketvo <ikxplicitie@gmail.com>
2023-04-17 15:49:58 +0700

    Updated quang's SecretKeyGuesser

--- src/main/java/vn/rmit/cosc2658/quang_algo1/SecretKeyGuesser.java
+++ src/main/java/vn/rmit/cosc2658/quang_algo1/SecretKeyGuesser.java
@@ -8,12 +8,19 @@ public class SecretKeyGuesser {
     private static final int LENGTH = 16;
 
     public static void start(SecretKey sk) {
-        // Count the number of occurrences for each character in the secret key
-        int[] letterCount = new int[4];
+        int[] letterCount = new int[4];  // Count the number of occurrences for each character in the secret key
+
         letterCount[0] = sk.guess("RRRRRRRRRRRRRRRR");
+        if (letterCount[0] == 16) return;
+
         letterCount[1] = sk.guess("MMMMMMMMMMMMMMMM");
+        if (letterCount[1] == 16) return;
+
         letterCount[2] = sk.guess("IIIIIIIIIIIIIIII");
+        if (letterCount[2] == 16) return;
+
         letterCount[3] = sk.guess("TTTTTTTTTTTTTTTT");
+        if (letterCount[3] == 16) return;
 
 
         // create a guess with exact number of each element in correct key
