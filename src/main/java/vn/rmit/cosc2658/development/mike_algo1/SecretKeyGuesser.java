bdf045e7e9fa786615becbba6c70ac21bc53a872
miketvo <ikxplicitie@gmail.com>
2023-04-19 15:06:41 +0700

    Moderate code readability improvement

--- src/main/java/vn/rmit/cosc2658/development/mike_algo1/SecretKeyGuesser.java
+++ src/main/java/vn/rmit/cosc2658/development/mike_algo1/SecretKeyGuesser.java
@@ -4,26 +4,36 @@ import vn.rmit.cosc2658.development.SecretKey;
 
 
 public class SecretKeyGuesser {
-    private static final char[] CHAR = "RMIT".toCharArray();  // Possible letters.
+    private static final char[] CHAR = "RMIT".toCharArray();     // Possible letters
+    private static final int[] charFreq = new int[CHAR.length];  // Number of occurrences (frequency) for each possible character
+    private static int totalFreq = 0;
+    private static int mostCommonCharHash = 0;                   // For optimization purposes. Initial assumption: R
 
 
+    /**
+     * @param secretKey The secret key to be guessed.
+     * @param secretKeyLength Length of the secret key.
+     * @param verbose Switch for verbose output. Defaults to <strong>{@code false}</strong>.
+     * @return The correct guess for the secret key.
+     */
     public static String start(SecretKey secretKey, int secretKeyLength, boolean verbose) {
-        int[] charFreq = new int[CHAR.length];  // Store the number of occurrences (frequency) for each character R, M, I, and T
+        /* **********************
 
-        int matchCount, totalFreq = 0, mostCommonCharHash = 0;
-        for (
-                // Stop if reaching T, or if the total number of occurrences has reached 16 already, to save us more
-                // SecretKey.guess() calls.
-                int charHash = 0;
-                totalFreq < secretKeyLength && charHash < CHAR.length - 1;
-                charHash++
-        ) {
+        Initial Guesses
+        ===============
+
+        - Time complexity: O(1)
+        - Space complexity: O(1)
+
+        ********************** */
+
+        // Step 1: Find the frequency of each character R, M and I (except T - it will be calculated below). 3 guesses.
+        for (int charHash = 0; totalFreq < secretKeyLength && charHash < CHAR.length - 1; charHash++) {
             String guess = Character.toString(CHAR[charHash]).repeat(secretKeyLength);
-            matchCount = secretKey.guess(guess);
-            if (verbose) System.out.printf("Guessing \"%s\", %d match...\n", guess, matchCount);
 
+            int matchCount = secretKey.guess(guess);
+            if (verbose) System.out.printf("Guessing \"%s\", %d match...\n", guess, matchCount);
             if (matchCount == secretKeyLength) {
-                // Early termination for edge cases of keys that contains only 1 repeating character.
                 if (verbose) System.out.printf("I found the secret key. It is \"%s\"\n", guess);
                 return guess;
             }
@@ -33,57 +43,53 @@ public class SecretKeyGuesser {
             if (charHash != 0 && charFreq[mostCommonCharHash] < matchCount) mostCommonCharHash = charHash;
         }
 
+        // Step 2: totalFreq == 0 means key contains only the character T. Saves us 1 SecretKey.guess() call.
         if (totalFreq == 0) {
-            // No occurrences of all other characters means the key contains only the character T. This saves us 1
-            // SecretKey.guess() call.
             String guess = "T".repeat(secretKeyLength);
             if (verbose) System.out.printf("I found the secret key. It is \"%s\"\n", guess);
             return guess;
         }
 
-        if (totalFreq < secretKeyLength) {
-            // The frequency of the last character T can be obtained by simply subtracting the key length by the total
-            // number of occurrences of the other characters. This saves us 1 SecretKey.guess() call.
-            charFreq[CHAR.length - 1] = secretKeyLength - totalFreq;
-        }
+        // Step 3: Otherwise, Frequency of T = secretKeyLength - totalFreq. Saves us 1 SecretKey.guess() call.
+        if (totalFreq < secretKeyLength) charFreq[CHAR.length - 1] = secretKeyLength - totalFreq;
 
 
-        // Baseline guess is a string filled with the most common character (the one with the highest frequency). This
-        // will save us more SecretKey.guess() calls, as our Main algorithm below would not have to call
-        // SecretKey.guess() for:
-        //     - Characters that we know are not in the key (frequency equal to 0 after the above steps);
-        //     - Multiple incorrect guesses the same index.
+        /* *************************************************************************************************************
+
+        General Case "Linear Character Swap" Guessing Algorithm
+        =======================================================
+
+        - Time complexity: O(n)
+        - Space complexity: O(n)
+
+        Start with a baseline guess which is a string filled with the most common character (the one with the highest
+        frequency). Then go through all other possible characters and check whether it can be used to replace any of the
+        characters in the baseline guess until it has correctly guessed the entire secret key.
+
+        This will save us more SecretKey.guess() calls, because our we would not have to call SecretKey.guess() for:
+            1. Characters that we know are not in the key (frequency equal to 0 after the above steps);
+            2. Multiple incorrect guesses on the same index.
+
+        ************************************************************************************************************* */
         char[] guess = Character.toString(CHAR[mostCommonCharHash]).repeat(secretKeyLength).toCharArray();
-        matchCount = charFreq[mostCommonCharHash];
-        boolean[] correct = new boolean[secretKeyLength];  // Assume that no correct character has been found
+        int cumulativeMatchCount = charFreq[mostCommonCharHash];
+        boolean[] correct = new boolean[secretKeyLength];
 
-        // Main algorithm
-        for (
-                // Consider the remaining characters
-                int charHash = (mostCommonCharHash + 1) % CHAR.length;
-                charHash != mostCommonCharHash;
-                charHash = (charHash + 1) % CHAR.length
-        ) {
-            for (
-                    // Linear search: Consider each character index of the key from left to right to be replaced with
-                    // one of the remaining characters. Stop early if we have used up our replacing character.
-                    int index = 0;
-                    charFreq[charHash] > 0 && index < secretKeyLength;
-                    index++
-            ) {
-                if (correct[index]) continue;  // Skip if we know we have found the correct character for this position
+        for (int charHash = (mostCommonCharHash + 1) % CHAR.length; charHash != mostCommonCharHash; charHash = (charHash + 1) % CHAR.length) {
+            for (int index = 0; charFreq[charHash] > 0 && index < secretKeyLength; index++) {
+                if (correct[index]) continue;
 
 
                 char originalChar = guess[index];
                 guess[index] = CHAR[charHash];
-                int newMatchCount = secretKey.guess(String.valueOf(guess));
-                if (verbose) System.out.printf("Guessing \"%s\", %d match...\n", String.valueOf(guess), matchCount);
+                int matchCount = secretKey.guess(String.valueOf(guess));
+                if (verbose) System.out.printf("Guessing \"%s\", %d match...\n", String.valueOf(guess), cumulativeMatchCount);
 
-                switch (newMatchCount - matchCount) {
+                switch (matchCount - cumulativeMatchCount) {
                     case 1 -> {  // Found the correct character for this position: New replacement character
                         correct[index] = true;
                         charFreq[charHash]--;
-                        matchCount = newMatchCount;
+                        cumulativeMatchCount = matchCount;
                     }
                     case -1 -> {  // Found the correct character for this position: Original baseline guess
                         correct[index] = true;
