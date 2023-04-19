7c083ce608d3892928f8a480d13fa01303a19c4a
miketvo <ikxplicitie@gmail.com>
2023-04-19 16:07:07 +0700

    Refactor: Changed SecretKeyGuesser.totalFreq to SecretKeyGuesser.totalCharFreq

--- src/main/java/vn/rmit/cosc2658/development/mike_algo1/SecretKeyGuesser.java
+++ src/main/java/vn/rmit/cosc2658/development/mike_algo1/SecretKeyGuesser.java
@@ -6,7 +6,7 @@ import vn.rmit.cosc2658.development.SecretKey;
 public class SecretKeyGuesser {
     private static final char[] CHAR = "RMIT".toCharArray();     // Possible letters
     private static final int[] charFreq = new int[CHAR.length];  // Number of occurrences (frequency) for each possible character
-    private static int totalFreq = 0;
+    private static int totalCharFreq = 0;                        // The sum of charFreq
     private static int mostCommonCharHash = 0;                   // For optimization purposes. Initial assumption: R
 
 
@@ -28,7 +28,7 @@ public class SecretKeyGuesser {
         ********************** */
 
         // Step 1: Find the frequency of each character R, M and I (except T - it will be calculated below). 3 guesses.
-        for (int charHash = 0; totalFreq < secretKeyLength && charHash < CHAR.length - 1; charHash++) {
+        for (int charHash = 0; totalCharFreq < secretKeyLength && charHash < CHAR.length - 1; charHash++) {
             String guess = Character.toString(CHAR[charHash]).repeat(secretKeyLength);
 
             int matchCount = secretKey.guess(guess);
@@ -39,19 +39,19 @@ public class SecretKeyGuesser {
             }
 
             charFreq[charHash] = matchCount;
-            totalFreq += matchCount;
+            totalCharFreq += matchCount;
             if (charHash != 0 && charFreq[mostCommonCharHash] < matchCount) mostCommonCharHash = charHash;
         }
 
-        // Step 2: totalFreq == 0 means key contains only the character T. Saves us 1 SecretKey.guess() call.
-        if (totalFreq == 0) {
+        // Step 2: totalCharFreq == 0 means key contains only the character T. Saves us 1 SecretKey.guess() call.
+        if (totalCharFreq == 0) {
             String guess = "T".repeat(secretKeyLength);
             if (verbose) System.out.printf("I found the secret key. It is \"%s\"\n", guess);
             return guess;
         }
 
-        // Step 3: Otherwise, Frequency of T = secretKeyLength - totalFreq. Saves us 1 SecretKey.guess() call.
-        if (totalFreq < secretKeyLength) charFreq[CHAR.length - 1] = secretKeyLength - totalFreq;
+        // Step 3: Otherwise, Frequency of T = secretKeyLength - totalCharFreq. Saves us 1 SecretKey.guess() call.
+        if (totalCharFreq < secretKeyLength) charFreq[CHAR.length - 1] = secretKeyLength - totalCharFreq;
 
 
         /* *************************************************************************************************************
